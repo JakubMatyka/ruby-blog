2cbc560698e696dabe01c6792ded9d99a85f949d
Jakub Matyka <jaqbec@gmail.com>
2023-10-26 00:10:31 +0200

    add resource to simplify CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
   get "up" => "rails/health#show", as: :rails_health_check
 end
