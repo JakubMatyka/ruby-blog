b073fb643805e428ef5adb3706a1c70cade7587c
Jakub Matyka <jaqbec@gmail.com>
2023-10-29 02:26:43 +0200

    generate a controller for a comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
-  resources :articles
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :articles do
+    resources :comments
+  end
 end
